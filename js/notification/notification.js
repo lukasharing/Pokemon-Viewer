6ccdf65c01aebe3fc5732170bc1025cea929e632
Lukas Haring <lukasharing@hotmail.com>
2018-06-20 15:22:17 +0200

    GUI And Hexadecimal Editor

--- js/notification/notification.js
+++ js/notification/notification.js
@@ -12,17 +12,19 @@
 const NotificationType = {
   NONE: 0,
   ERROR: 1,
-  NOTIFIACTION: 2,
+  SUCCESS: 2,
+  NOTIFICATION: 3,
 };
 
 class Notification{
-  constructor(_description, _type = NotificationType.NONE){
+  constructor(_title, _description, _type = NotificationType.NONE){
     // TODO: WIKI INFORMATION.
     this.id = Utils.uid();
 
     // Basic Information
-    this.type = _type;
+    this.title = _title;
     this.description = _description;
+    this.type = _type;
 
     // Date emitted.
     this.date = new Date();
@@ -48,26 +50,19 @@ class Notification{
     let minutes = Utils.pad(this.date.getMinutes().toString(), '0', 2);
     let hours = Utils.pad(this.date.getHours().toString(), '0', 2);
 
-    let image;
-    switch(this.type){
-      case NotificationType.NONE: image = null; break;
-      case NotificationType.ERROR: image = "cross"; break;
-      case NotificationType.NOTIFIACTION: image = "bell"; break;
-      default: image = "http://via.placeholder.com/75x75";
+    // Find out the name of the type.
+    let notification_name = "NONE";
+    for(let notification in NotificationType){
+      if(NotificationType[notification] == this.type){
+        notification_name = notification;
+      }
     }
 
     let html =
-    `<div class="notification" data-uid="${ this.id }">
-      <div class="notifiaction-close">&#10005;</div>`;
-    if(image !== null){
-      html += `<div class="notification-image">
-                 <img src="./css/images/notification/${image}.png" width="75" height="75" alt="Notification" title="Notification" />
-               </div>
-              `;
-    }
-    html += `
+    `<div class="notification notification--${ notification_name }" data-uid="${ this.id }">
+      <div class="notifiaction-close">&#10005;</div>
       <div class="notification-info">
-        <h6>Notification Title 2</h6>
+        <h6>${ this.title }</h6>
         <p>${ this.description }</p>
         <div class="notifiaction-footer">
           <a href="#" class="notifiaction-hlink">+ Read more</a>
